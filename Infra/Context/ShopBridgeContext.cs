16358342b3ed8a59cc3f6d1f5c2f3bd2e11eb96f
Ariel Lisboa Lopes <101740188+ArielLopes888@users.noreply.github.com>
2023-03-21 16:34:46 -0300

    add context and map

---
+++ Infra/Context/ShopBridgeContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Context
+{
+    public class ShopBridgeContext : DbContext
+    {
+        public ShopBridgeContext() 
+        {}
+
+        public ShopBridgeContext(DbContextOptions<ShopBridgeContext> options) : base(options) 
+        {}
+
+        public virtual DbSet<Products> Products { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder.ApplyConfiguration(new UserMap());
+        }
+
+    }
+}
---
+++ Infra/Mappings/ProductMap.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Mappings
+{
+    public class ProductMap : IEntityTypeConfiguration<Products>
+    {
+        public void Configure(EntityTypeBuilder<Products> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+                .UseIdentityColumn()
+                .HasColumnType("BIGINT");
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(80)
+                .HasColumnName("Name")
+                .HasColumnType("VARCHAR(80)");
+
+            builder.Property(x => x.Description)
+                .IsRequired()
+                .HasMaxLength(180)
+                .HasColumnName("description")
+                .HasColumnType("VARCHAR(180)");
+
+            builder.Property(x => x.Price)
+                .IsRequired()
+                .HasMaxLength(18)
+                .HasColumnName("price")
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(x => x.ImageUrl)
+               .IsRequired()
+               .HasMaxLength(180)
+               .HasColumnName("imageUrl")
+               .HasColumnType("VARCHAR(180)");
+
+            builder.Property(x => x.Category)
+               .IsRequired()
+               .HasMaxLength(180)
+               .HasColumnName("category")
+               .HasColumnType("VARCHAR(180)");
+        }
+    }
+}
