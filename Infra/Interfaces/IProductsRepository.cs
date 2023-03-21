bc9b81846dd5f71a8e33786278416d8c94dbff4b
Ariel Lisboa Lopes <101740188+ArielLopes888@users.noreply.github.com>
2023-03-21 17:04:29 -0300

    add products repository and interface

---
+++ Infra/Interfaces/IProductsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Infra.Interfaces
+{
+    public interface IProductsRepository : IBaseRepository<Products>
+    {
+        Task<List<Products>> SearchByName (string name);
+        Task<List<Products>> SearchByCategory (string category);
+
+    }
+}
---
+++ Infra/Repositories/ProductsRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Infra.Context;
+using Infra.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositories
+{
+    public class ProductsRepository : BaseRepository<Products>, IProductsRepository
+    {
+        private readonly ShopBridgeContext _context;
+        public ProductsRepository(ShopBridgeContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Products>> SearchByName(string name)
+        {
+            var allProducts = await _context.Products
+               .Where
+               (
+                   x => x.Name.ToLower().Contains(name.ToLower())
+               )
+               .AsNoTracking()
+               .ToListAsync();
+
+            return allProducts;
+        }
+
+        public async Task<List<Products>> SearchByCategory(string category)
+        {
+            var allProducts = await _context.Products
+               .Where
+               (
+                   x => x.Category.ToLower().Contains(category.ToLower())
+               )
+               .AsNoTracking()
+               .ToListAsync();
+
+            return allProducts;
+        }
+    }
+}
