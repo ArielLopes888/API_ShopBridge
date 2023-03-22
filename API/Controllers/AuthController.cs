01ed3dc62767f3e17a9ffa5a4cd680442d9ed973
Ariel Lisboa Lopes <101740188+ArielLopes888@users.noreply.github.com>
2023-03-21 23:15:49 -0300

    add authentication controller and start product controller

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using API.Token;
+using API.Utilities;
+using API.ViewModels;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace API.Controllers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ITokenGenerator _tokenGenerator;
+
+        public AuthController(IConfiguration configuration, ITokenGenerator tokenGenerator)
+        {
+            _configuration = configuration;
+            _tokenGenerator = tokenGenerator;
+        }
+
+        [HttpPost]
+        [Route("/api/vi/auth/login")]
+        public IActionResult Login([FromBody] LoginViewModel loginViewModel)
+        {
+            try
+            {
+                var tokenLogin = _configuration["Jwt:Login"];
+                var tokenPassword = _configuration["Jwt:Password"];
+
+                if (loginViewModel.Login == tokenLogin && loginViewModel.Password == tokenPassword)
+                {
+                    return Ok(new ResultViewModel
+                    {
+                        Message = "Admin successfully authenticated!",
+                        Success = true,
+                        Data = new
+                        {
+                            Token = _tokenGenerator.GenerateToken(),
+                            TokenExpires = DateTime.UtcNow.AddHours(int.Parse(_configuration["Jwt:HoursToExpire"]))
+                        }
+                    });
+                }
+                else
+                {
+                    return StatusCode(401, Responses.UnauthorizedErrorMessage());
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, Responses.ApplicationErrorMessage());
+            }
+        }
+    }
+}
