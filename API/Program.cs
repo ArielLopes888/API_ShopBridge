db843c05a340b94c21ff1231c95ee76658a16a8c
Ariel Lisboa Lopes <101740188+ArielLopes888@users.noreply.github.com>
2023-03-22 18:03:33 -0300

    azure configurations

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,17 @@ namespace API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>{})
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    if (context.HostingEnvironment.IsProduction())
+                    {
+                        var builtConfig = config.Build();
+                        config.AddAzureKeyVault(
+                            builtConfig["AzureKeyVault:Vault"],
+                            builtConfig["AzureKeyVault:ClientId"],
+                            builtConfig["AzureKeyVault:ClientSecret"]);
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
